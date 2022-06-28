5131b96411c10bb8a2890913fe37990b16d0b883
henjinic <henjinic@gmail.com>
2022-06-29 05:09:02 +0900

    feat: draft get_patch_isolation

--- biotools/habitat.py
+++ biotools/habitat.py
@@ -1,7 +1,11 @@
+import arcpy.analysis
+import arcpy.management
+import pandas as pd
+
 from biotools import arcutils
 
 
-def get_habitat_size(biotope_layer, lower_bounds=[50, 10, 1, 0], scores=[1, 0.5, 0.3, 0.2, 0]):
+def get_habitat_size(biotope_layer, lower_bounds=(50, 10, 1, 0), scores=(1, 0.5, 0.3, 0.2, 0)):
     """서식지 규모
 
     ### To Do
@@ -18,3 +22,43 @@ def range_evaluate(value, lower_bounds, scores):
         if value >= lower_bound:
             return scores[i]
     return scores[-1]
+
+
+def get_default_habitable_codes():
+    result = []
+    for category, upper_bound in zip("HIJKLM", [12, 1, 13, 2, 5, 4]):
+        result += [category + str(i) for i in range(1, upper_bound + 1)]
+    return tuple(result)
+
+def get_patch_isolation(biotope_layer, habitable_codes=get_default_habitable_codes()):
+    """패치고립도
+    """
+    query = " OR ".join([f"비오톱 = '{code}'" for code in habitable_codes])
+    arcpy.management.SelectLayerByAttribute(biotope_layer, "NEW_SELECTION", query)
+
+    buffer_layer = arcpy.analysis.Buffer(biotope_layer, "memory/buffer_layer", "125 Meters", "FULL", "ROUND", "NONE")
+    arcpy.management.CalculateGeometryAttributes(buffer_layer, [["Buf_Area", "AREA_GEODESIC"]], area_unit="SQUARE_METERS")
+
+    in_buffer_table = arcpy.analysis.TabulateIntersection(buffer_layer, "ORIG_FID", biotope_layer,
+                                                          "memory/in_buffer_table", "FID", out_units="SQUARE_METERS")
+
+    arcpy.management.SelectLayerByAttribute(biotope_layer, "CLEAR_SELECTION")
+
+    biotope_df = arcutils.layer_to_df(biotope_layer)
+    buffer_df = arcutils.layer_to_df(buffer_layer)
+    in_buffer_df = arcutils.layer_to_df(in_buffer_table)
+
+    in_buffer_area_s = in_buffer_df.groupby("ORIG_FID").sum()["AREA"]
+    buffer_df = pd.merge(buffer_df, in_buffer_area_s, on="ORIG_FID")
+    buffer_df = buffer_df.assign(PatchIsolation=lambda x: x["AREA"] * 100 / x["Buf_Area"])
+
+    merging_columns = ["ORIG_FID", "BUFF_DIST", "Buf_Area", "AREA", "PatchIsolation"]
+    buffer_df = buffer_df[merging_columns]
+
+    result_df = pd.merge(biotope_df, buffer_df, left_on="FID", right_on="ORIG_FID", how="left")
+    result_df = result_df.fillna({field: 0 for field in merging_columns[1:]})
+
+    arcpy.management.Delete("memory/buffer_layer")
+    arcpy.management.Delete("memory/in_buffer_table")
+
+    return result_df
--- example.py
+++ example.py
@@ -9,14 +9,17 @@ def main():
     survey_point_layer = arcpy.management.MakeFeatureLayer("path/to/survey_point.shp", "survey_point_layer")
     species_info_df = pd.read_csv("path/to/species_info.csv", encoding="cp949")
 
-    result = foodchain.get_number_of_food_resources(biotope_layer, survey_point_layer, species_info_df)
-    print(result.sort_values("All_Snumber", ascending=False).head())
+    result_df = foodchain.get_number_of_food_resources(biotope_layer, survey_point_layer, species_info_df)
+    print(result_df.sort_values("All_Snumber", ascending=False).head())
 
-    result = foodchain.get_number_of_food_resources(biotope_layer, survey_point_layer, species_info_df, one_per_point=False)
-    print(result.sort_values("All_Snumber", ascending=False).head())
+    result_df = foodchain.get_number_of_food_resources(biotope_layer, survey_point_layer, species_info_df, one_per_point=False)
+    print(result_df.sort_values("All_Snumber", ascending=False).head())
 
-    result = habitat.get_habitat_size(biotope_layer)
-    print(result.head())
+    result_df = habitat.get_habitat_size(biotope_layer)
+    print(result_df.head())
+
+    result_df = habitat.get_patch_isolation(biotope_layer)
+    print(result_df.sort_values("Buf_Area", ascending=False).head())
 
 
 if __name__ == "__main__":
